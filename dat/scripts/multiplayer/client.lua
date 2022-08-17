237f408bfccc759d9c4b49330b112380c2f4beda
Throstur <sparog@gmail.com>
2022-08-17 02:27:19 +0000

    fix client respawn

--- dat/scripts/multiplayer/client.lua
+++ dat/scripts/multiplayer/client.lua
@@ -178,7 +178,7 @@ client.spawn = function( ppid, shiptype, shipname , outfits, ai )
         pmem = client.pilots[ppid]:memory()
         pmem.comm_no = _("NOTICE: Staying in chat will get you killed or disconnected. Caveat user!")
         print("created pilot for " .. tostring(ppid))
-    elseif ppid == client.playerinfo.nick and not client.alive or shiptype ~= player.pilot():ship():nameRaw() then
+    elseif ppid == client.playerinfo.nick and ( not client.alive or shiptype ~= player.pilot():ship():nameRaw() ) then
 --      client.pilots[ppid] = player.pilot()
         -- the server tells us to spawn in a new ship or acknowledges this ship
         local mpshiplabel = "MPSHIP" .. tostring(rnd.rnd(10000, 99999)) .. shipname
